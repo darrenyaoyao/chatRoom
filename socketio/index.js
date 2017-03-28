b2d582b15827e72fd10e80454d9ffd4422437076
darrenyaoyao <b02901042@ntu.edu.tw>
2017-03-28 21:12:32 +0800

    change api

--- socketio/index.js
+++ socketio/index.js
@@ -7,59 +7,44 @@ module.exports = class SocketServer {
         this.updateNickname = this.updateNickname.bind(this);
         this.disconnect = this.disconnect.bind(this);
         this.newUser = this.newUser.bind(this);
-        this.chatMessage = this.chatMessage.bind(this);
+        this.chat = this.chat.bind(this);
         this.listen = this.listen.bind(this);
         this.getUsers = this.getUsers.bind(this);
     }
 
     updateNickname () {
-        this.io.sockets.emit('username', Object.keys(this.users));
+        this.io.sockets.emit('userID', Object.keys(this.users));
     }
 
     disconnect (socket) {
         console.log('user disconnected');
-        if ( !socket.nickname ) {
+        if ( !socket._id ) {
             return false;
         }
-        delete this.users[socket.nickname];
+        delete this.users[socket.id];
         this.updateNickname();
     }
 
     newUser (data, callback, socket) {
-        if (data in this.users) {
+        if (data._id in this.users) {
             callback(false);
         } else {
             callback(true);
-            socket.nickname = data;
-            this.users[socket.nickname] = socket;
+            socket._id = data._id;
+            this.users[socket._id] = socket;
             this.updateNickname();
         }
     }
 
-    chatMessage (data, callback, socket) {
-        var msg = data.trim();
-        if (msg.substr(0,3) === '/w ') {
-            msg = msg.substr(3);
-            var ind = msg.indexOf(' ');
-            if (ind !== -1) {
-                var name = msg.substring(0, ind);
-                msg = msg.substring(ind + 1);
-                if (name in this.users) {
-                    this.users[name].emit('whisper', {
-                        msg: msg,
-                        nickname: socket.nickname
-                    });
-                    console.log('Whisper!');
-                } else {
-                    callback('Error! Enter a valid user');
-                }
-            } else {
-                callback('Error! Please enter a message for your whisper.');
-            }
+    chat (data, callback, socket) {
+        if (data.receiver in this.users) {
+            this.users[data.receiver].emit('chat', {
+                msg: data.msg,
+                sender: socket._id
+            });
         } else {
-            this.io.emit('chat message', {
-                msg: msg,
-                nickname: socket.nickname
+            this.users[socket._id].emit('err', {
+                err: 'There is no this user'
             });
         }
     }
@@ -69,7 +54,7 @@ module.exports = class SocketServer {
             console.log('a user connected');
             socket.on('disconnect', (socket) => this.disconnect(socket));
             socket.on('newUser', (data, callback) => this.newUser(data, callback, socket));
-            socket.on('chat message', (data, callback) => this.chatMessage(data, callback, socket));
+            socket.on('chat', (data, callback) => this.chat(data, callback, socket));
         });
     }
 
